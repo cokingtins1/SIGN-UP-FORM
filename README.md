235f217fc5d71fb568fdb187724dcf59858dd8f8
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-06-18 15:52:23 -0400

    Final sign-up form

--- README.md
+++
@@ -1,3 +0,0 @@
-# SIGN-UP-FORM
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-md1nxf)
--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello!');
+const firstName = document.getElementById('first-name')
+const lastName = document.getElementById('last-name')
+const password = document.getElementById('password')
+const confirmPassword = document.getElementById('confirm-password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('password-error')
+
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    // if (firstName.value === '' || firstName.value == null) {
+    //     messages.push('name is required')
+    // }
+    if(password.value.length <=6) {
+        messages.push('Password must be longer than 6 characters')
+    }
+
+    if(confirmPassword.value = password.value) {
+        messages.push('Your passwords do not match')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})
+
+form.addEventListener('keyup', (e) =>{
+    
+})
